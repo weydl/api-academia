cb918b9483d134bb33c91bd708e2b80176e85bba
Weyd <weydlll2606@gmail.com>
2026-06-03 11:34:20 -0300

    implementa AlunoModel com criação e atualização de alunos

--- src/models/alunoModel.ts
+++ src/models/alunoModel.ts
@@ -1,26 +1,38 @@
-// ============================================================
-// 🎯 TAREFA: Crie o AlunoModel que HERDA do Repository!
-// ============================================================
-//
-// import { JsonRepository } from "../repositories/JsonRepository";
-// import { Aluno } from "../entities/Aluno";
-// import { CriarAlunoDTO, AtualizarAlunoDTO } from "../interfaces";
-//
-// export class AlunoModel extends JsonRepository<Aluno> {
-//   constructor() {
-//     super("dados/alunos.json", Aluno.fromJSON);
-//     // ↑ passa o caminho E a função de conversão!
-//   }
-//
-//   async criar(dados: CriarAlunoDTO): Promise<Aluno> {
-//     Aluno.validar(dados);  // validação na CLASSE!
-//     const aluno = new Aluno(0, dados.nome, dados.email, dados.plano, dados.mensalidade);
-//     return this.adicionar(aluno);
-//   }
-//
-//   async atualizarAluno(id, dados: AtualizarAlunoDTO): Promise<Aluno> {
-//     return this.atualizar(id, dados);
-//   }
-// }
-// ============================================================
+import { JsonRepository } from "../repositories/JsonRepository";
+import { Aluno } from "../entities/Aluno";
+import { CriarAlunoDTO, AtualizarAlunoDTO } from "../interfaces";
 
+export class AlunoModel extends JsonRepository<Aluno> {
+  constructor() {
+    super("dados/alunos.json", Aluno.fromJSON);
+  }
+
+  async criar(dados: CriarAlunoDTO): Promise<Aluno> {
+    Aluno.validar(dados);
+
+    const aluno = new Aluno(
+      0,
+      dados.nome,
+      dados.email,
+      dados.plano,
+      dados.mensalidade
+    );
+
+    await this.adicionar(aluno);
+
+    return aluno;
+  }
+
+  async atualizarAluno(
+    id: number,
+    dados: AtualizarAlunoDTO
+  ): Promise<Aluno> {
+    const aluno = await this.buscarPorId(id);
+
+    Object.assign(aluno, dados);
+
+    await this.atualizar(id, aluno);
+
+    return aluno;
+  }
+}
