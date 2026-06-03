b895eb5ef88aaf42c3cb837b31c63d615646ebc1
Weyd <weydlll2606@gmail.com>
2026-06-03 11:41:53 -0300

    registra middleware global de tratamento de erros

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { logger } from "./middlewares/logger";
 import { alunoRoutes } from "./routes/alunoRoutes";
-// 🎯 TODO: importar errorHandler de "./middlewares/errorHandler"
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -11,7 +12,8 @@ app.use(logger);
 
 app.use(alunoRoutes);
 
-// 🎯 TODO: app.use(errorHandler) — DEPOIS das rotas!
+// sempre depois das rotas
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("✅ API Academia rodando em http://localhost:3000");
